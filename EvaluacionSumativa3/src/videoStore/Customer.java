80e0deafee4e88b40a734bff2036434a439ca968
Julio Cesar <julio.142j@hotmail.com>
2015-05-31 21:37:58 -0600

    Extrayendo metodos "getAmount y getFrequentRenterPoints" a longMethod "Statement"

--- EvaluacionSumativa3/src/videoStore/Customer.java
+++ EvaluacionSumativa3/src/videoStore/Customer.java
@@ -37,31 +37,10 @@ public class Customer {
 			Rental each			= (Rental) it.next();
 			
 			//determine amounts for each line
-			switch (each.getMovie().getPriceCode())
-			{
-				case Movie.REGULAR: 
-					thisAmount += 2;
-					if (each.getDaysRented() > 2)
-						thisAmount += (each.getDaysRented()-2) * 1.5;
-					break;
-				
-				case Movie.NEW_RELEASE:
-					thisAmount += each.getDaysRented() * 3;
-					break;
-					
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (each.getDaysRented()>3)
-						thisAmount += (each.getDaysRented()-3) * 1.5;
-					break;
-			}
+			thisAmount = getAmount(thisAmount, each);
 			
-			//add frequent renter points
-			frequentRenterPoints++;
-			
-			//add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode()==Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints = getFrequentRenterPoints(
+					frequentRenterPoints, each);
 			
 			//show figures for this rental
 			result+= "\t" + each.getMovie().getTitle() + "\t"+String.valueOf(thisAmount) + "\n";
@@ -73,6 +52,38 @@ public class Customer {
 		
 		return result;
 		
+	}
+
+	public int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+		//add frequent renter points
+		frequentRenterPoints++;
+		
+		//add bonus for a two day new release rental
+		if ((each.getMovie().getPriceCode()==Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
+
+	public double getAmount(double thisAmount, Rental each) {
+		switch (each.getMovie().getPriceCode())
+		{
+			case Movie.REGULAR: 
+				thisAmount += 2;
+				if (each.getDaysRented() > 2)
+					thisAmount += (each.getDaysRented()-2) * 1.5;
+				break;
+			
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+				
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented()>3)
+					thisAmount += (each.getDaysRented()-3) * 1.5;
+				break;
+		}
+		return thisAmount;
 	}
 
 	private String addFooterLines(double totalAmount, int frequentRenterPoints,
